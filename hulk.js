dd815997d0359d54c254620f2315a01dc8045be7
Kevin Burke <kevin@twilio.com>
2013-07-16 23:51:46 -0700

    recursive dictionaries are handled

--- hulk.js
+++ hulk.js
@@ -9,24 +9,44 @@
         return button;
     };
 
-    $.hulk = function(selector, data, callback) {
-        var html = $(document.createElement('div'));
+    /**
+     * Convert a JSON object into HTML
+     *
+     * This function calls itself recursively
+     */
+    var convertMapToHTML = function(data) {
+        if (typeof data === "string") {
+            return data;
+        }
+
+        var map = $(document.createElement('div'));
+        map.addClass('hulk-map');
         for (var key in data) {
             var pair = $(document.createElement('div'));
-            pair.addClass('hulk-map');
+            pair.addClass('hulk-map-pair');
 
             var keyHtml = $(document.createElement('input'));
             keyHtml.addClass('hulk-map-key');
             keyHtml.attr('value', key);
             pair.append(keyHtml);
 
-            var valueHtml = $(document.createElement('input'));
-            valueHtml.addClass('hulk-map-value');
-            valueHtml.attr('value', data[key]);
+            var valueHtml;
+            if (typeof data[key] === "string") {
+                valueHtml = $(document.createElement('input'));
+                valueHtml.addClass('hulk-map-value');
+                valueHtml.attr('value', convertMapToHTML(data[key]));
+            } else {
+                valueHtml = convertMapToHTML(data[key]);
+            }
             pair.append(valueHtml);
-            html.append(pair);
 
+            map.append(pair);
         }
+        return map;
+    };
+
+    $.hulk = function(selector, data, callback) {
+        var html = convertMapToHTML(data);
         html.append(getSaveButton());
         $(selector).html(html);
     };

cff5969791be43fcd3ca036697179965d51649d9
Kevin Burke <kevin@twilio.com>
2013-07-24 11:15:24 -0700

    set up 'grunt watch' to lint, compile js

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,51 +1,47 @@
 module.exports = function(grunt) {
     grunt.initConfig({
-      jshint: {
-        all: ['Gruntfile.js', 'hulk.js'],
-        options: {
-            "forin": true,
-            "latedef": true,
-            "unused": true,
-            "trailing": true
-        }
-      },
-
-      uglify: {
-        options: {
-            mangle: {
-                except: ['jQuery']
-            },
-            compress: true
+        jshint: {
+            all: ['Gruntfile.js', 'hulk.js'],
+            options: {
+                "forin": true,
+                "latedef": true,
+                "unused": true,
+                "trailing": true
+            }
         },
-        my_target: {
-            files: {
-                'hulk.min.js': ['hulk.js']
+
+        uglify: {
+            options: {
+                mangle: {
+                    except: ['jQuery']
+                },
+                compress: true
+            },
+            my_target: {
+                files: {
+                    'hulk.min.js': ['hulk.js']
+                }
+            }
+        },
+
+        qunit: {
+            files: ['tests/test.html']
+        },
+
+        sass: {
+            dist: {
+                files: {
+                    'hulk-all.css': 'hulk-all.scss'
+                }
+            }
+        },
+
+        watch: {
+            scripts: {
+                files: 'hulk.js',
+                tasks: ['jshint', 'uglify']
             }
         }
-      },
-
-      qunit: {
-        files: ['tests/test.html']
-      },
-
-      sass: {
-          dev: {
-              src: 'hulk.scss',
-              dest: 'hulk.css'
-          },
-          colors: {
-            src: 'hulk-colors.scss',
-            dest: 'hulk-colors.css'
-        }
-      },
-
-      watch: {
-          sass: {
-              // We watch and compile sass files as normal but don't live reload here
-              files: ['hulk.scss', 'hulk-colors.scss'],
-              tasks: ['sass'],
-          }
-      }
     });
 
     grunt.loadNpmTasks('grunt-contrib-sass');
